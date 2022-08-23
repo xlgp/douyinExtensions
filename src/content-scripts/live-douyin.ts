a213e63d1aa7b0641ec276b19703daebd5aaf763
hzsn-csh <hzsn-csh@qq.com>
2022-08-23 09:58:57 +0800

    feat:新增点击action添加组件

--- src/content-scripts/live-douyin.ts
+++ src/content-scripts/live-douyin.ts
@@ -1,7 +1,23 @@
+import { App } from 'vue';
 import {createVueApp} from './useVue';
 
-let videoDom:HTMLVideoElement = document.getElementsByTagName('video')[0];
+const getVideo = ()=>{return document.getElementsByTagName('video')[0];}
 
+let videoDom:HTMLVideoElement = getVideo();
+let app:App<Element> = undefined as unknown as App;
 if (videoDom) {
-    createVueApp(videoDom);
+    app = createVueApp(videoDom);
 }
+
+chrome.runtime.onMessage.addListener(
+    (
+      message: any,
+      sender: chrome.runtime.MessageSender,
+      sendResponse: (response?: any) => void
+    ) => {
+      if (message.key == 'create' && !app) { //创建
+        app = createVueApp(getVideo());
+      }
+      sendResponse("ok");
+    }
+  );
