140c3aea80f304ea54f7aa807a58c69c4c84fe14
hzsn-csh <hzsn-csh@qq.com>
2022-11-06 11:17:31 +0800

    构建0.0.6

---
+++ dist/content-scripts/douyin-plugin.js
@@ -0,0 +1,6 @@
+(function () {
+	'use strict';
+
+	function l(){var t,e;(e=(t=document.getElementById("_douyin_live_scroll_container_"))==null?void 0:t.previousSibling)==null||e.remove(),document.getElementsByTagName("footer")[0].style.display="none";}function o(t){var e,n,r,i;(n=(e=t.firstElementChild)==null?void 0:e.firstElementChild)==null||n.remove(),(i=(r=t.firstElementChild)==null?void 0:r.lastElementChild)==null||i.remove();}window.addEventListener("load",()=>{var t;l();try{let e=document.querySelector("[data-e2e='living-container']");(t=e==null?void 0:e.nextSibling)==null||t.remove(),o(e);}catch{}});
+
+})();
