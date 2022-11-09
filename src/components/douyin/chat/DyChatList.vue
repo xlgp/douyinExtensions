24cfe3679cf40a2d4aacffca8999a7a1a69c928e
hzsn-csh <hzsn-csh@qq.com>
2022-11-09 11:20:36 +0800

    进度条滚到底

--- src/components/douyin/chat/DyChatList.vue
+++ src/components/douyin/chat/DyChatList.vue
@@ -1,11 +1,20 @@
 <template>
-  <el-scrollbar height="300px">
-    <DyChatItem v-for="item in items" :data="item" />
+  <el-scrollbar ref="scrollbarRef" height="300px">
+    <DyChatItem @vnode-mounted="handleChildMounted" v-for="item in items" :data="item" />
   </el-scrollbar>
 </template>
 <script setup lang="ts">
 import { useChatStore } from "./store/Chat";
 
+const scrollbarRef = ref();
+
 const chatStore = useChatStore();
 const items = chatStore.filterChatItems;
+
+const handleChildMounted = () => {
+  let scrollHeight = scrollbarRef.value.wrap$.scrollHeight;
+  scrollbarRef.value.setScrollTop(scrollHeight);
+};
+
+onMounted(() => {});
 </script>
