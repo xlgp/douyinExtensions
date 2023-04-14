346c4779aeff999576182954c26ccd924cb92ff7
hzsn-csh <hzsn-csh@qq.com>
2023-04-14 13:57:15 +0800

    refactor: 重构元素渲染后执行

--- src/content-scripts/live-douyin.ts
+++ src/content-scripts/live-douyin.ts
@@ -1,4 +1,5 @@
 import { App } from 'vue';
+import useMutationObserver from './useMutationObserver';
 import { createEmojiVueApp, createDyExVueApp, createTimeVueApp } from './useVue';
 
 let app: App<Element> = undefined as unknown as App;
@@ -8,8 +9,10 @@ let timeApp: App<Element> = undefined as unknown as App;
 window.addEventListener('load', () => {
   setTimeout(() => {
     app = createDyExVueApp();
+  }, 5000);
+  useMutationObserver.then(node => {
     timeApp = createTimeVueApp();
     emojiApp = createEmojiVueApp();
-  }, 5000);
+  }).catch(console.error);
 
 });
---
+++ src/content-scripts/useMutationObserver.ts
@@ -0,0 +1,43 @@
+
+// 观察器的配置（需要观察什么变动）
+const config = { childList: true, subtree: true };
+
+function isWebCastChatRoom(node: Element) {
+    return typeof node.className === "string" && node.className.includes("webcast-chatroom");
+}
+
+const p = new Promise((resolve, reject) => {
+
+    // 当观察到变动时执行的回调函数
+    const callback = function (mutationsList: MutationRecord[], observer: MutationObserver) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+
+                for (const node of mutation.addedNodes) {
+
+                    if (isWebCastChatRoom(node as Element)) {
+                        observer.disconnect();
+                        resolve(node);
+                        break;
+                    }
+                }
+            }
+        }
+    };
+
+    // 创建一个观察器实例并传入回调函数
+    const observer = new MutationObserver(callback);
+
+    // 选择需要观察变动的节点
+    let targetNode = document.querySelector(
+        "[data-e2e='living-container']"
+    );
+    // 以上述配置开始观察目标节点
+    if (targetNode) {
+        observer.observe(targetNode, config);
+    } else {
+        reject("没有找到 targetNode [data-e2e='living-container']");
+    }
+});
+
+export default p;
