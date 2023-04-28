14ceba80fe7a73b348940d0e7e0703f4a00ea5d5
hzsn-csh <hzsn-csh@qq.com>
2023-04-28 09:11:18 +0800

    fix: 修复页面出现异常后不创建vue的bug

--- src/content-scripts/live-douyin.ts
+++ src/content-scripts/live-douyin.ts
@@ -7,11 +7,29 @@ let emojiApp: App<Element> = undefined as unknown as App;
 let timeApp: App<Element> = undefined as unknown as App;
 
 window.addEventListener('load', () => {
- 
-  useMutationObserver.then(node => {
+
+  function createVueApp() {
     timeApp = createTimeVueApp();
     emojiApp = createEmojiVueApp();
     app = createDyExVueApp();
-  }).catch(console.error);
+  }
+
+  let p1 = useMutationObserver.catch(err => {
+    console.error(err);
+    return p2;
+  });
+
+  let p2 = new Promise((resolve, reject) => {
+    console.log(`5s 后执行`);
+    setTimeout(() => {
+      resolve(5000);
+    }, 5000)
+  });
+
+  //先监听聊天区dom创建，再创建vue。
+  //若监听失败，5s后执行创建操作。
+  Promise.race([p1, p2])
+    .then(createVueApp)
+    .catch(console.error);
 
 });
