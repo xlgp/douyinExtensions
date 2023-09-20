1d1b30af9bdf906af1abb99e90f8c6373871ee5f
hzsn-csh <hzsn-csh@qq.com>
2023-09-20 13:24:19 +0800

    fix: 改插件图标的位置

--- src/content-scripts/useCreateElement.ts
+++ src/content-scripts/useCreateElement.ts
@@ -11,13 +11,13 @@ function getDySideBar() {
 
 let style: Style = {
   position: "fixed",
-  bottom: "8px",
-  right: "8px",
+  bottom: "25%",
+  right: "2px",
   "z-index": zIndex,
 };
 
-function createElement(id:string) {
-   let mountEl = document.getElementById(id);
+function createElement(id: string) {
+  let mountEl = document.getElementById(id);
   if (mountEl) {
     mountEl.remove();
   }
@@ -27,50 +27,44 @@ function createElement(id:string) {
   return mountEl;
 }
 
-function createDyExtElement(){
+function createDyExtElement() {
   const MOUNT_EL_ID = "dy-ext-box";
   let mountEl = createElement(MOUNT_EL_ID);
-  let sidebarElem = getDySideBar();
-  if (sidebarElem) {
-    sidebarElem.style.zIndex = zIndex;
-    sidebarElem.appendChild(mountEl);
-  } else {
-    setElemStyle(mountEl, style);
-    document.body.appendChild(mountEl);
-  }
+  setElemStyle(mountEl, style);
+  document.body.appendChild(mountEl);
   return mountEl;
 }
 
-function createZimuElement(){
+function createZimuElement() {
   const id = "dy-zimu-box";
   const div = createElement(id);
-  let style:Style = {
-    display:"flex",
-    position:"fixed",
-    'z-index':zIndex
+  let style: Style = {
+    display: "flex",
+    position: "fixed",
+    'z-index': zIndex
   };
   setElemStyle(div, style);
   document.body.append(div);
   return div;
 }
 
-function createTimeElement(){
+function createTimeElement() {
   const id = "dy-time-box";
   const div = createElement(id);
-  let style:Style = {
-    display:"flex",
-    position:"absolute",
-    bottom:"0px",
-    right:"16px",
-    'z-index':zIndex,
+  let style: Style = {
+    display: "flex",
+    position: "absolute",
+    bottom: "0px",
+    right: "16px",
+    'z-index': zIndex,
     color: 'var(--color-text-2)',
     'font-size': 'small',
   };
   let Elem = document.querySelector(".webcast-chatroom");
   setElemStyle(div, style);
-  
+
   Elem?.append(div);
   return div;
 }
 
-export { createDyExtElement , createZimuElement, createTimeElement};
+export { createDyExtElement, createZimuElement, createTimeElement };
